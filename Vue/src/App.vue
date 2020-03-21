e7a55566ad9fa1841ff1fbaec76d7d57d7884c4b
Celine Balmes <comptes.98@gmail.com>
2020-03-21 10:51:40 +0100

    feat(Vue): add vue.js part of the application with an association page connected to backend. Created wit pwa template. Use bootstrap, vue-cli, axios

---
+++ Vue/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <header>
+      <span><router-link :to="{ name: 'Hello'}">Associations manager</router-link></span>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+header a {
+  color: white;
+}
+
+header a:hover {
+  color: white;
+  text-decoration: none;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
---
+++ Vue/src/components/AssociationsList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+  	<ul>
+  		<li v-for="association in associations">
+  			<b>{{ association.Name }}</b></br>
+  			<p>{{ association.Description }}</p>
+  		</li>
+  	</ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'associationsList',
+  data () {
+    return {
+      associations: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://localhost:44302/api/associations')
+      .then(response => (this.associations = response.data))
+      .catch(error => console.log(error))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
